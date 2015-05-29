68ce6b48970f54b836289b7df3df7f00cf63b826
Reid Ransom <reid@reidransom.com>
2015-05-29 11:50:58 -0400

    Include detailed user progress data

--- common/djangoapps/defy/views.py
+++ common/djangoapps/defy/views.py
@@ -8,12 +8,57 @@ import courseware
 
 from .decorators import lcms_only
 
-def dumps(value):
-    """ Prepare value for JSON serialization.
+
+class NormalizeData(object):
+
+    SERIALIZABLE_TYPES = [str, int, bool, float, list, dict, type(None)]
+
+    def __init__(self, element):
+        self.element = element
+
+    def execute(self):
+        if isinstance(self.element, dict):
+            self.iterate_dict()
+        if isinstance(self.element, list):
+            self.iterate_list()
+        else:
+            return
+
+    def normalize(self, el):
+
+        #if isinstance(el, <ClassName>):
+        #    # Do custom serialization here
+        #    return el
+
+        # Check if the object has a `__norm()` function for custom normalization.
+        if hasattr(el, '__norm'):
+            return el.__norm()
+
+        if type(el) not in self.SERIALIZABLE_TYPES:
+            return str(el)
+        return el
+
+    def iterate_list(self):
+        for i in range(len(self.element)):
+            self.element[i] = self.normalize(self.element[i])
+            node = NormalizeData(self.element[i])
+            node.execute()
+
+    def iterate_dict(self):
+        for key in self.element:
+            self.element[key] = self.normalize(self.element[key])
+            node = NormalizeData(self.element[key])
+            node.execute()
+
+def dumps(data):
+    """ JSON serialize any object.
     """
-    if value is not None:
-        return str(value)
-    return value
+    norm = NormalizeData(data)
+    norm.execute()
+    try:
+        return json.dumps(norm.element)
+    except TypeError:
+        return str(norm.element)
 
 @lcms_only
 def courses(request):
@@ -22,11 +67,11 @@ def courses(request):
     courses = []
     for course in branding.get_visible_courses():
         data = {
-            'def_id': dumps(course.scope_ids.def_id),
+            'def_id': course.scope_ids.def_id,
             'org':    course.org,
             'number': course.number,
-            'start':  dumps(course.start),
-            'end':    dumps(course.end),
+            'start':  course.start,
+            'end':    course.end,
         }
         about_keys = [
             'overview',
@@ -48,22 +93,37 @@ def courses(request):
         ]
         data['about'] = {key: courseware.courses.get_course_about_section(course, key) for key in about_keys}
         courses.append(data)
-    return HttpResponse(json.dumps(courses), content_type='application/json')
+    return HttpResponse(dumps(courses), content_type='application/json')
 
 def student_progress(request):
     """ Return a json response with student progress data.
     """
+
+    # Make courses accessible by module_id in a dict
+    courses = {}
+    for course in branding.get_visible_courses():
+        module_id = dumps(course.scope_ids.def_id)
+        courses[module_id] = course
+
     data = []
     modules = courseware.models.StudentModule.objects.filter(module_type='course')
     for module in modules:
+
+        # Get detailed user progress data
+        module_id = dumps(module.module_state_key)
+        course = courses[module_id]
+        courseware_summary = courseware.grades.progress_summary(module.student, request, course)
+
         data.append({
             'email':       module.student.email,
             'module_type': module.module_type,
-            'module_id':   dumps(module.module_state_key),
+            'module_id':   module_id,
             'state':       json.loads(module.state),
-            'created':     dumps(module.created),
-            'modified':    dumps(module.modified),
-            'course_id':   dumps(module.course_id),
+            'created':     module.created,
+            'modified':    module.modified,
+            'course_id':   module.course_id,
+            'summary':     courseware.grades.progress_summary(module.student, request, course),
         })
-    return HttpResponse(json.dumps(data), content_type='application/json')
+
+    return HttpResponse(dumps(data), content_type='application/json')
 
