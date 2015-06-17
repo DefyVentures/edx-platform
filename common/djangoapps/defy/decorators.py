d9e88102011992c26b38edbb4948c25fae71aa40
reidransom <reid@reidransom.com>
2015-06-17 13:04:57 -0400

    Fix lcms only context_processor

--- common/djangoapps/defy/decorators.py
+++ common/djangoapps/defy/decorators.py
@@ -4,7 +4,7 @@ from django.http import Http404
 def lcms_only(view_func):
     def authorize(request, *args, **kwargs):
         user_ip = request.META['REMOTE_ADDR']
-        if not getattr(settings, 'DEFY_LCMS_IP') or settings.DEFY_LCMS_IPS == user_ip:
+        if not getattr(settings, 'DEFY_LCMS_IP') or settings.DEFY_LCMS_IP == user_ip:
             return view_func(request, *args, **kwargs)
         raise Http404
     return authorize
