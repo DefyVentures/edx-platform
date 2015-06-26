d1eb112bb5f7dafe4fe52c8c293d43454bbf1a72
Reid Ransom <reid@reidransom.com>
2015-06-26 15:35:41 -0400

    added ssl

--- lms/envs/defy.py
+++ lms/envs/defy.py
@@ -1,6 +1,6 @@
 from .aws import defyenv
 
-DEFY_LCMS_BASE_URL = 'http://learn.defyventures.org'
+DEFY_LCMS_BASE_URL = 'https://learn.defyventures.org'
 
 # A list of client IPs that can access views decorated with @lcms_only
 DEFY_LCMS_IP = '10.0.0.64'
